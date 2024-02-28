4cca8ca5a74f10e086606fd23c431b3c3ecb77e5
Souvik Biswas <156061293+souvikbiswas2802@users.noreply.github.com>
2024-02-28 06:54:10 +0000

    added day 4

---
+++ Codes/Day004.py
@@ -0,0 +1,9 @@
+print("\033[34m","Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!", "\033[0m")
+
+name = input("What is your name? ")
+enemy = input("What is your worst enemy’s name? ")
+super_power = input("What is your superpower? ")
+live = input("Where do you live? ")
+food = input("What is your favorite food? ")
+
+print("Hello", "\033[31m", name, "\033[0m", "! Your ability to", "\033[31m", super_power, "\033[0m", "will make sure you never have to look at", "\033[31m", enemy, "\033[0m", "again. Go eat", "\033[31m", food,"\033[0m", "as you walk down the streets of", "\033[31m", live, "\033[0m", "and use", "\033[31m", super_power, "\033[0m", "for good and not evil!")
