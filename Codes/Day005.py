3b44cc0cb079060200b04f3d835df3f4d206de20
Souvik Biswas <156061293+souvikbiswas2802@users.noreply.github.com>
2024-02-29 12:31:09 +0530

    Create Day005.py
    added day 5

---
+++ Codes/Day005.py
@@ -0,0 +1,20 @@
+print("Marvel Charecter Creator")
+print("--")
+print()
+haround = input("Do you like 'hanging around? >")
+if haround == "yes":
+  print("You are spiderman")
+else:
+  print("You are not spiderman")
+print()
+gravelly = input("Do you have a 'gravelly' voice? >")
+if gravelly == "yes":
+  print("You are Korg")
+else:
+  print("You are not Korg")
+print()
+marvelous = input("Do you often feel 'Marvelous'? >")
+if marvelous == "yes":
+  print("You are Captain Marvel")
+else:
+  print("You are not Captain Marvel")
