e3cd553d0ad564778e3838a4fcb2dd926fd28b39
Souvik Biswas <156061293+souvikbiswas2802@users.noreply.github.com>
2024-03-01 12:23:16 +0530

    Create Day006.py
    Day 6 added

---
+++ Codes/Day006.py
@@ -0,0 +1,21 @@
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+username = input("Username > ")
+password = input("Password > ")
+if username == "Souvik" and password == "7ouvik":
+  print("Why hello there Souvik, what a lovely accent you have, you could have charmed your way in here even without a password.")
+  print()
+  print("Have a great day.")
+  
+elif username == "Steve" and password == "minecraft" :
+  print("Why hello there Steve, what a lovely accent you have, you could have charmed your way in here even without a password.")
+  print()
+  print("Have a great day.")
+  
+elif username == "James" and password == "bond" :
+  print("Why hello there James, what a lovely accent you have, you could have charmed your way in here even without a password.")
+  print()
+  print("Have a great day.")
+
+else: 
+  print("Sorry! You are not allowed to access this system.")
