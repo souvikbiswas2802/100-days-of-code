9cbbde481f4b9836bf1b212a3c89372cf3f77bc2
Souvik Biswas <156061293+souvikbiswas2802@users.noreply.github.com>
2024-03-02 14:33:03 +0530

    Create Day007.py

---
+++ Codes/Day007.py
@@ -0,0 +1,89 @@
+print("👮Fake Anime Fan Finder👀")
+print("------------------------")
+anime = input("What's your favourite Anime? ")
+if anime == "One piece":
+  print("Oh really?!")
+  character = input("Name me any of the Straw Hat Pirates? ")
+  
+  if character == "Nami":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is her job on the ship? ")
+    if job == "Navigator":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+  elif character == "Luffy":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is his job on the ship? ")
+    if job == "Captain":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+  elif character == "Zoro":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is his job on the ship? ")
+    if job == "Swordsman":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+  elif character == "Sanji":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is his job on the ship? ")
+    if job == "Cook":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+  elif character == "Robin":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is her job on the ship? ")
+    if job == "Archeologist":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+  elif character == "Chopper":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is his job on the ship? ")
+    if job == "Doctor":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+  elif character == "Franky":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is his job on the ship? ")
+    if job == "Shipwright":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+  elif character == "Brook":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is his job on the ship? ")
+    if job == "Musician":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+  elif character == "Jinbe":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is his job on the ship? ")
+    if job == "Helmsman":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+  elif character == "Ussop":
+    print("You got that by pure chance.")
+    job = input("Okay then, what is his job on the ship? ")
+    if job == "Sniper":
+      print("You are a true fan!")
+    else:
+      print("See! FAKE One piece fan!")
+
+else:
+  print("Nah! One Piece is better.")
