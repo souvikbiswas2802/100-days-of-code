a14eee8ce3355e53adf05c7084cdea4138b95552
Souvik Biswas <156061293+souvikbiswas2802@users.noreply.github.com>
2024-03-03 12:57:10 +0530

    Create Day008.py
    Day 8 added

---
+++ Codes/Day008.py
@@ -0,0 +1,93 @@
+print("\033[34m","Welcome to Daily Affirmation Generator ")
+print("----------------------------------------", "\033[0m")
+print()
+name = input("What is your name? ")
+print()
+day = input("What day is it today? ")
+print()
+if day == "Monday" or day == "monday":
+  print("Hello", name, ", it's Monday, the first day of the week, you got this! I believe in you!")
+  feel = input("On a scale of 1-10, how do you feel today? ")
+  if feel == "1" or feel == "2" or feel == "3" or feel == "4":
+    print(name, "Don't be sad, you are amazing! Be happy!")
+  elif feel == "5" or feel == "6" or feel == "7":
+    print(name, "You are doing great, keep going! Don't give up!")
+  elif feel == "8" or feel == "9" or feel == "10":
+    print(name, "You are amazing, keep up the good work! You will do great!")
+  else:
+    print("Please enter a number between 1-10")
+
+elif day == "Tuesday" or day == "tuesday":
+  print("Hello", name, ", it's Tuesday, the second day of the week, you got this! I believe in you!")
+  feel = input("On a scale of 1-10, how do you feel today? ")
+  if feel == "1" or feel == "2" or feel == "3" or feel == "4":
+    print(name, "Don't be sad, you are amazing! Be happy!")
+  elif feel == "5" or feel == "6" or feel == "7":
+    print(name, "You are doing great, keep going! Don't give up!")
+  elif feel == "8" or feel == "9" or feel == "10":
+    print(name, "You are amazing, keep up the good work! You will do great!")
+  else:
+    print("Please enter a number between 1-10")
+
+elif day == "Wednesday" or day == "wednesday":
+  print("Hello", name, ", it's Wednesday, the third day of the week, you got this! I believe in you!")
+  feel = input("On a scale of 1-10, how do you feel today? ")
+  if feel == "1" or feel == "2" or feel == "3" or feel == "4":
+    print(name, "Don't be sad, you are amazing! Be happy!")
+  elif feel == "5" or feel == "6" or feel == "7":
+    print(name, "You are doing great, keep going! Don't give up!")
+  elif feel == "8" or feel == "9" or feel == "10":
+    print(name, "You are amazing, keep up the good work! You will do great!")
+  else:
+    print("Please enter a number between 1-10")
+
+elif day == "Thursday" or day == "thursday":
+  print("Hello", name, ", it's Thursday, the fourth day of the week, you got this! I believe in you!")
+  feel = input("On a scale of 1-10, how do you feel today? ")
+  if feel == "1" or feel == "2" or feel == "3" or feel == "4":
+    print(name, "Don't be sad, you are amazing! Be happy!")
+  elif feel == "5" or feel == "6" or feel == "7":
+    print(name, "You are doing great, keep going! Don't give up!")
+  elif feel == "8" or feel == "9" or feel == "10":
+    print(name, "You are amazing, keep up the good work! You will do great!")
+  else:
+    print("Please enter a number between 1-10")
+
+elif day == "Friday" or day == "friday":
+  print("Hello", name, ", it's Friday, the fifth day of the week, you got this! I believe in you!")
+  feel = input("On a scale of 1-10, how do you feel today? ")
+  if feel == "1" or feel == "2" or feel == "3" or feel == "4":
+    print(name, "Don't be sad, you are amazing! Be happy!")
+  elif feel == "5" or feel == "6" or feel == "7":
+    print(name, "You are doing great, keep going! Don't give up!")
+  elif feel == "8" or feel == "9" or feel == "10":
+    print(name, "You are amazing, keep up the good work! You will do great!")
+  else:
+    print("Please enter a number between 1-10")
+
+elif day == "Saturday" or day == "saturday":
+  print("Hello", name, ", it's Saturday, the sixth day of the week, you got this! I believe in you!")
+  feel = input("On a scale of 1-10, how do you feel today? ")
+  if feel == "1" or feel == "2" or feel == "3" or feel == "4":
+    print(name, "Don't be sad, you are amazing! Be happy!")
+  elif feel == "5" or feel == "6" or feel == "7":
+    print(name, "You are doing great, keep going! Don't give up!")
+  elif feel == "8" or feel == "9" or feel == "10":
+    print(name, "You are amazing, keep up the good work! You will do great!")
+  else:
+    print("Please enter a number between 1-10")
+
+elif day == "Sunday" or day == "sunday":
+  print("Hello", name, ", it's Sunday, the seventh day of the week, you got this! I believe in you!")
+  feel = input("On a scale of 1-10, how do you feel today? ")
+  if feel == "1" or feel == "2" or feel == "3" or feel == "4":
+    print(name, "Don't be sad, you are amazing! Be happy!")
+  elif feel == "5" or feel == "6" or feel == "7":
+    print(name, "You are doing great, keep going! Don't give up!")
+  elif feel == "8" or feel == "9" or feel == "10":
+    print(name, "You are amazing, keep up the good work! You will do great!")
+  else:
+    print("Please enter a number between 1-10")
+
+else:
+  print("Please enter a day of the week")
