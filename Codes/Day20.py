87fe66ad514fd024ae04a8b6ccd23a879b44ac58
Souvik Biswas <156061293+souvikbiswas2802@users.noreply.github.com>
2024-03-15 19:52:48 +0530

    Create Day20.py

---
+++ Codes/Day20.py
@@ -0,0 +1,8 @@
+print("List Generator")
+print()
+start = int(input("Start at: "))
+end = int(input("End before: "))
+increment = int(input("Increment between values: "))
+print()
+for i in range(start, end, increment):
+  print(i)
