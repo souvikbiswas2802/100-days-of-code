a24c35b948d2c45d0289ff8ed98b38f6e3c4dd14
Souvik Biswas <156061293+souvikbiswas2802@users.noreply.github.com>
2024-03-25 20:15:17 +0530

    Delete Day030.py

--- Day030.py
+++
@@ -1,9 +0,0 @@
-print("30 Days Down - What did you think?")
-print()
-for i in range(1, 31):
-  thought = input(f"Day {i}:\n")
-  print()
-  myText = f"You thought Day {i} was"
-  print(f"{myText:^30}")
-  print(f"{thought:^30}")
-  print()
