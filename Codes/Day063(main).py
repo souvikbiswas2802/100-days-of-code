fcc05f6ca5d21d64a43810b2878389cdc0923295
Souvik Biswas <156061293+souvikbiswas2802@users.noreply.github.com>
2024-04-27 18:07:17 +0530

    Create Day063(main).py

---
+++ Codes/Day063(main).py
@@ -0,0 +1,12 @@
+import Day063(myLibrary) as ml
+
+text = "Hello World\n"
+ml.textcolor("red", text)
+ml.wait(1)
+ml.textcolor("green", text)
+ml.wait(1)
+ml.textcolor("yellow", text)
+ml.wait(1)
+ml.print_hearts(5,1)
+ml.wait(1)
+print(ml.rollDice())
